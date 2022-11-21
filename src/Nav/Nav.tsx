8f8cb881cec6e87a7fc43359820af6b71bbe10da
Ekaterina <kattekly@mail.ru>
2022-11-21 15:26:11 +0300

    Create Nav

---
+++ src/Nav/Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Nav.module.css'
+
+const Nav = () => {
+    return (
+        <div className={s.nav}>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+
+        </div>
+    );
+};
+
+export default Nav;
