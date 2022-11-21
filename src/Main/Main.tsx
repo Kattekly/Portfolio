690bd3630870e9bcf70cd898421787e5a4fa442a
Ekaterina <kattekly@mail.ru>
2022-11-21 16:32:30 +0300

    Create Main

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -4,7 +4,14 @@ import s from './Main.module.css'
 const Main = () => {
     return (
         <div className={s.mainBlock}>
-
+            <div className={s.container}>
+                <div className={s.text}>
+                    <span>Hi There</span>
+                    <h1>Name</h1>
+                    <p>Frontend-разработчик</p>
+                </div>
+                <div className={s.photo}></div>
+            </div>
         </div>
     );
 };
