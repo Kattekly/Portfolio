89a282e058798809f5ce77ce38679a2841bc2c84
Ekaterina <kattekly@mail.ru>
2022-11-22 13:37:58 +0300

    Create Skills

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import Skills from "./Skills/Skills";
 
 function App() {
     return (
         <div>
             <Header/>
             <Main/>
+            <Skills/>
         </div>
     );
 }
---
+++ src/Skills/Skills.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Skills.module.css'
+import styleContainer from "../Common/Styles/Container.module.css";
+
+const Skills = () => {
+    return (
+        <div className={s.skillsBlock}>
+<div className={styleContainer.container}>
+    <h2>Skills</h2>
+</div>
+        </div>
+    );
+};
+
+export default Skills;
