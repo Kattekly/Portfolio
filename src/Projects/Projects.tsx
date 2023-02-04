47e39506bcb0774203f13af126ce103059d4a74d
Ekaterina <kattekly@mail.ru>
2023-02-04 12:59:13 +0300

    scss

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -5,6 +5,7 @@ import Project from './Project/Project';
 import Title from '../Common/Components/Title/Title';
 import todoImage from './../Assets/Images/Todo.jpg'
 import socialImage from './../Assets/Images/Social.jpg'
+import cardsImage from './../Assets/Images/Cards.jpg'
 
 const Projects = () => {
     const social = {
@@ -15,13 +16,21 @@ const Projects = () => {
         backgroundImage: `url(${todoImage})`
     }
 
+    const cards = {
+        backgroundImage: `url(${cardsImage})`
+    }
+
     return (
         <div className={s.projectBlock}>
             <div className={`${styleContainer.container} ${s.projectContainer}`}>
                 <Title text={"Projects"}/>
                 <div className={s.projects}>
-                    <Project style={social} title={"Social Network"} description={"Описание 1 bvdcvbjsdbkjsncklsmxkldsnk nbdjsbchsdcbh bhbhbhb njnjnjn bhbhb njnjnjn gggg"}/>
-                    <Project style={todo} title={"Todolist"} description={"Описание  ghsvchsdbvchjsbcjshncjnskjdc njdsbncjbdchj njnjbvgvcfcf bhbbhhbhb sj"}/>
+                    <Project style={todo} title={"Todolist"}
+                             description={"Описание  ghsvchsdbvchjsbcjshncjnskjdc njdsbncjbdchj njnjbvgvcfcf bhbbhhbhb sj"}/>
+                    <Project style={social} title={"Social Network"}
+                             description={"Описание 1 bvdcvbjsdbkjsncklsmxkldsnk nbdjsbchsdcbh bhbhbhb njnjnjn bhbhb njnjnjn gggg"}/>
+                    <Project style={cards} title={"Cards"}
+                             description={"Описание  ghsvchsdbvchjsbcjshncjnskjdc njdsbncjbdchj njnjbvgvcfcf bhbbhhbhb sj bhbhbh"}/>
                 </div>
             </div>
         </div>
