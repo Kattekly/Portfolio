af9b2759d3d86c4aa44aad33dbbd195e19d63907
Ekaterina <kattekly@mail.ru>
2023-02-12 10:06:38 +0300

    scss

--- src/DistantWork/DistantWork.tsx
+++ src/DistantWork/DistantWork.tsx
@@ -5,10 +5,10 @@ import s from "./DistantWork.module.css";
 const DistantWork = () => {
     return (
         <div className={s.block}>
-        <div className={`${styleContainer.container} ${s.workContainer}`}>
-            <h2>Рассматриваю только удаленную работу</h2>
-            <button className={s.button}>Нанять меня</button>
-        </div>
+            <div className={`${styleContainer.container} ${s.workContainer}`}>
+                <h2>Рассматриваю только удаленную работу</h2>
+                <button className={s.button}>Нанять меня</button>
+            </div>
         </div>
     );
 };
