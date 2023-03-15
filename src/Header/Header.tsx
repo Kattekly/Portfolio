eff54619a9bc132efaf79bed4cbab5373a67322c
Ekaterina <kattekly@mail.ru>
2023-03-15 10:00:58 +0300

    scss

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import s from './Header.module.css'
+import React, {useEffect, useState} from 'react';
+import s from './Header.module.scss'
 import {Nav} from "../Nav/Nav";
-
+import styleContainer from '../Common/Styles/Container.module.css'
 
 const Header = () => {
+   /* const [position, setPosition] = useState(window.scrollY)
+    const [visible, setVisible] = useState(true)
+    useEffect(() => {
+        const handleScroll = () => {
+            let moving = window.scrollY
+            setVisible(position > moving);
+            setPosition(moving)
+        };
+        window.addEventListener("scroll", handleScroll);
+        return (() => {
+            window.removeEventListener("scroll", handleScroll);
+        })
+    })
+
+    const cls = visible ? s.visible : s.hidden;*/
+
     return (
-        <div className={s.header}>
-            <Nav/>
+        <div className={`${s.headerBlock}`}>
+            <div className={`${styleContainer.container} ${s.header}`}>
+                <div className={s.container}>
+                    <Nav/>
+                </div>
+            </div>
         </div>
-    );
+
+    )
 };
 
 export default Header;
