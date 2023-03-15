1e1a59b344443374b9f2dc2e16281c1599eeba0b
Ekaterina <kattekly@mail.ru>
2023-03-15 15:19:52 +0300

    footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -5,21 +5,20 @@ import Title from "../Common/Components/Title/Title";
 import vk from "../Assets/Images/icons8-vk-circled-96.png"
 import telegram from "../Assets/Images/icons8-telegram-app-96.png"
 import github from "../Assets/Images/github.png"
+import codewars from "../Assets/Images/codewars.png"
 
 const Footer = () => {
     return (
-        // <div className={s.footerBlock}>
-        //     <div className={`${styleContainer.container} ${s.footerContainer}`}>
         <div className={s.footer}>
             <div className={s.container}>
-                <Title title={"My name"}/>
+                <Title title={"Ekaterina Kozachenko"}/>
                 <div className={s.iconsContainer}>
                     <a href="https://vk.com/kattekly"><img src={vk} className={s.pictures} alt="VK"/></a>
                     <a href="https://t.me/K_t_r_n_k"><img src={telegram} alt="telegram"/></a>
                     <a href="https://github.com/Kattekly"><img src={github} alt="github"/></a>
-                    <a href=""><img src={''} alt="linkedin"/></a>
+                    <a href="https://www.codewars.com/users/Kattekly"><img src={codewars} alt="codewars"/></a>
                 </div>
-                <div className={s.span}>&copy; 2023 Все права защищены</div>
+                <div className={s.span}>&copy; 2023 All Rights Reserved</div>
             </div>
         </div>
     );
