6fddc6c32d4306fd96704f32fe03718e5668c9e8
Ekaterina <kattekly@mail.ru>
2023-03-16 11:27:21 +0300

    style

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import s from './Main.module.scss'
-import styleContainer from './../Common/Styles/Container.module.css'
+import {Fade} from "react-awesome-reveal";
+import ReactTypingEffect from "react-typing-effect";
+import {Particle} from "./Particle";
+import Tilt from 'react-parallax-tilt';
+import mainPhoto from "../Assets/Images/KozachenkoE.jpg"
 
 const Main = () => {
+
     return (
-        <div className={s.mainBlock}>
-            <div className={styleContainer.container}>
-                <div className={s.text}>
-                    <span>Hi There</span>
-                    <h1>Name</h1>
-                    <p>Frontend-разработчик</p>
+        <div id={'main'} >
+            <section className={`${s.mainContainer} ${s.container}`}>
+                <Particle/>
+                <div className={s.about}>
+                    <Fade cascade>
+                        <span> HELLO, MY NAME IS </span>
+                        <h1> {'Ekaterina Kozachenko'} </h1>
+                        <ReactTypingEffect text={'FRONTEND DEVELOPER'}/>
+                    </Fade>
                 </div>
-                <div className={s.photo}></div>
-            </div>
+                <Tilt>
+                    <img src={mainPhoto} className={s.photo} alt='portfolioMainImg'/>
+                </Tilt>
+
+            </section>
         </div>
+
     );
 };
 
