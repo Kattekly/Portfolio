62216428e4d769b1520375e0bfaefe1491f79697
Ekaterina <kattekly@mail.ru>
2023-03-20 16:04:16 +0300

    feat: create modal

--- src/Contacts/ContactForm.tsx
+++ src/Contacts/ContactForm.tsx
@@ -1,17 +1,54 @@
 import {Button} from "../Common/Components/Button/Button";
-import {useContactForm} from "./useContactForm";
+import {EMAIL_SERVICE, useContactForm, Values} from "./useContactForm";
 import {Input} from "../Common/Components/Input/Input";
 import {FC, useState} from "react";
 import s from './Contacts.module.scss'
 import {SnackBar} from "../Common/Components/SnackBar/SnackBar";
 import {Modal} from "../Common/Components/Modal/Modal";
+import {useFormik} from "formik";
+import {validationSchema} from "../Common/utils/formValidators";
+import emailjs from "@emailjs/browser";
 
 
 export const ContactForm: FC = () => {
-    const {formik, onDisabled, snackbarShow, snackbarType, snackbarMessage} = useContactForm()
+    const [onDisabled, setOnDisabled] = useState<boolean>(false)
+    const [isOpenModal, setIsOpenModal] = useState(true)
+
+    const formik = useFormik<Values>({
+        initialValues: {
+            name: '',
+            email: '',
+            message: ''
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values, actions) => {
+            setOnDisabled(true)
+            emailjs.send(EMAIL_SERVICE.serviceID, EMAIL_SERVICE.templateID, {
+                name: values.name,
+                email: values.email,
+                message: values.message
+            }, EMAIL_SERVICE.publicKey)
+                .then((res) => {
+                    setIsOpenModal(true)
+                    actions.resetForm()
+                })
+                .catch((e) => {
+
+                })
+                .finally(() => {
+                    setOnDisabled(false)
+                    document.body.style.overflow = 'unset';
+                })
+        },
+    })
+
+    const handleClose = () => {
+        setIsOpenModal(false)
+    }
 
     return (
         <>
+            {isOpenModal ? <Modal handleClose={handleClose}/> : ''}
             <form  className={s.form} onSubmit={formik.handleSubmit}>
                 <Input label={formik.touched.name && formik.errors.name}
                        placeholder='Your name'
@@ -44,7 +81,7 @@ export const ContactForm: FC = () => {
                 />
                 <Button title='Send' type='submit' disable={onDisabled}/>
             </form>
-            <SnackBar type={snackbarType} message={snackbarMessage} show={snackbarShow}/>
+            {/*<SnackBar type={snackbarType} message={snackbarMessage} show={snackbarShow}/>*/}
         </>
     )
 }
