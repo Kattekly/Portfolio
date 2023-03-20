f1b8ad6def4d00d2f09126cb68088d973b7815ca
Ekaterina <kattekly@mail.ru>
2023-03-20 15:20:20 +0300

    feat: create form contact

--- src/DistantWork/DistantWork.tsx
+++ src/DistantWork/DistantWork.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import styleContainer from "../Common/Styles/Container.module.css";
 import s from "./DistantWork.module.scss";
 import {Button} from "../Common/Components/Button/Button";
-import {animateScroll} from "react-scroll";
+import { Link} from "react-scroll";
 
 export const DistantWork = () => {
     return (
         <div className={s.block}>
             <div className={`${styleContainer.container} ${s.workContainer}`}>
                 <h2 className={s.text}>I AM AVAILABLE FOR FREELANCE AND REMOTE WORK</h2>
-                <Button title={'Employ'} onClick={() => animateScroll.scrollToBottom()}>
-                    default
-                </Button>
+                <Link to="contactForm"
+                      activeClass={s.active}
+                      spy={true}
+                      smooth={true}
+                      offset={-50}
+                      duration={1000}
+                      className={s.link}>
+                    <Button title={'Hire'}>
+                        default
+                    </Button>
+                </Link>
             </div>
         </div>
     );
